7e5cdbeee1f5917b73dfcdab38cce20069047abe
Daniel <dte.biotech46@gmail.com>
2026-03-17 13:33:01 -0400

    Added UBun core functionality

--- README.md
+++ README.md
@@ -7,13 +7,25 @@ A website template that bundles Tailwind utility classes to make styling faster
 3. Tailwind
 
 # How to Run
+1. Fork this repo into your favorite IDE
+2. Then run these commands:
+```bash
+cd UBun_Dir
+bun run dev
+```
 
 # How it Works
-
-# Notes
-1. Don't run bunconfig.sh, just copy the commands after you create a new directory where a bun command must be run
+1. Groups of utility classes are defined as string constants 
+2. These string constants are referenced in the className property of each HTML element
+3. To use multiple utility class bundles, use the classnames() function and list each constant out
 
 # Sources
 [Bun](https://bun.com/docs)
-[TailwindCSS](https://tailwindcss.com/docs/installation/framework-guides/nextjs)
+
+[TailwindCSS](https://tailwindcss.com)
+
 [DevTo: Add Tailwind to a React and Vite project](https://dev.to/mosnyik/how-to-add-tailwindcss-to-a-react-app-built-with-vite-2025-guide-24oi)
+
+[React Multiple ClassNames](https://www.dhiwise.com/post/the-definitive-handbook-to-react-multiple-class-name)
+
+[How to center buttons in Tailwind](https://tailkits.com/blog/tailwind-css-centering/#centering-buttons)
--- UBun_Dir/src/App.tsx
+++ UBun_Dir/src/App.tsx
@@ -1,26 +1,46 @@
 import { useState } from 'react'
 import './App.css'
 import './index.css'
+import classnames from "classnames"
 
 function App() {
   const [count, setCount] = useState(0)
+  const h1 = "text-2xl"
+  const h2 = "text-xl"
+  const h3 = "text-lg"
+
+  const center = "place-items-center place-self-center"
+  const button = "text-center"
 
   return (
     <>
-      <h1>Hello world!</h1>
-      <h2>These have the same colors</h2>
-      <h3>This is a template</h3>
-
-      <p>Same font styles</p>
-      <p>Same font styles</p>
-      <p>Same font styles</p>
-
-      <div>
-          <p>Same border settings and colors</p>
+      <div className={ classnames( center ) }>
+        <br></br>
+        <h1 className={ classnames( h1 ) }>Hello world!</h1>
+        <h2 className={ classnames( h2 ) }>Welcome to UBun</h2>
+        <h3 className={ classnames( h3 ) }>Feel free to fork this template</h3>
+      <br></br>
       </div>
 
-      <div>
-          <p>Same border settings and colors</p>
+      <h3 className={ classnames( h1, center ) }>Why UBun?</h3>
+      <p className={ classnames( center ) }>Tailwind is better than regular CSS</p>
+      <p className={ classnames( center ) }>But for projects with lots utility classes that are shared across multiple elements</p>
+
+      <br></br>
+
+      <div className={ classnames( center ) }>
+          <p>Tailwind can easily waste time</p>
+      </div>
+
+      <div className={ classnames( center ) }>
+          <p>So why not store shared utility classes in native Javascript variables?</p>
+      </div>
+
+      <br></br>
+
+      <div className={ classnames( button ) }>
+        <button>Now with UBun... (click to continue)</button><br></br>
+        <button>Have a good day! (click to continue)</button>
       </div>
     </>
   )
